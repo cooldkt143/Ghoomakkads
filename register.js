eb911ce557d23eb983e999533f01caca0997a9f7
cooldkt143 <deepakkumartripathy1008@gmail.com>
2024-11-21 15:51:05 +0530

    editng pages

---
+++ register.js
@@ -0,0 +1,4 @@
+//Linking Travel Host Community page
+function  openTravelHost(){
+    window.location.href = "travelhostcommunity.html";
+}
---
+++ travelhostcommunity.js
@@ -0,0 +1,14 @@
+//linking destination page 
+function openDestination(){
+    window.location.href= "destination.html";
+}
+
+// linking contact us page 
+function openContact(){
+    window.location.href="ContactUs.html";
+}
+
+//linking tour page 
+function openTour(){
+    window.location.href="tourpage.html";
+}
